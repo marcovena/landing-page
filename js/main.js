5b5ff6b089a4e10e5cb19a300b8430a44df6c1cb
marcovena <marcovena@libero.it>
2011-06-13 19:15:38 +0200

    updated favicon; added docs for jQuery, changed columns in footer

--- js/main.js
+++ js/main.js
@@ -1,6 +1,26 @@
 $(document).ready( function() {
 	$('.welcome').click( function (event) {
 		$(this).fadeOut('slow');
-		
 	});
+	$('.column ul').hide();
+
+	$('#home-footer .column').hover( function() {
+		
+		items	= $(this).children('ul');
+		items.slideDown('slow');
+
+	}, function() {
+		$('#home-footer .column ul').slideUp('slow');
+	} );
+	$()
+
+	/*
+	 $('p.menus').mouseout(function(){
+	 $("div.menuitems'").slideUp("slow");
+	 $('p').css({backgroundImage:""}); });
+	 $('p.menus').mouseover(function(){ $(this).css({'background-image':"url(down.png)", 'background-repeat':"no-repeat",
+	 'background-position':"right"}).next("div.menuitems").slideDown(500)
+	 .siblings("div.menuitems").slideUp("slow"); $(this).siblings().css({backgroundImage:""});
+	 */
+
 });
